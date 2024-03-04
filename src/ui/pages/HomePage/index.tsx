0622572abd7aa2dc5f581bf742b3aa0d9c6dd6af
saeed mohammdiha <saeed1440124@gmail.com>
2024-03-04 15:52:41 +0330

    export menuList from home page and change the name of the file to index

---
+++ src/ui/pages/HomePage/index.tsx
@@ -0,0 +1,40 @@
+import { Grid, useTheme } from "@mui/material";
+import { useMemo } from "react";
+import { useTranslation } from "react-i18next";
+import Menu from "ui/components/Menu";
+import Title from "ui/components/Title";
+import BoxAdapter from "ui/htsc-components/BoxAdapter";
+import BreadcrumbsAdapter from "ui/htsc-components/BreadcrumbsAdapter";
+import { menuList } from "./menuList";
+
+export default function HomePage() {
+  const { t } = useTranslation();
+  const theme = useTheme();
+
+  const breadcrumbs = useMemo(() => {
+    return [
+      {
+        title: t("accountServices"),
+        key: "1",
+        href: "/"
+      },
+      {
+        title: t("electronicCheck"),
+        key: "2",
+        href: "/"
+      }
+    ];
+  }, []);
+
+  return (
+    <div style={{ height: "100%" }}>
+      <BoxAdapter fullWidth>
+        <BreadcrumbsAdapter breadcrumbs={breadcrumbs} />
+        <Title>{t("chequeMenu")}</Title>
+        <Grid dir={theme.direction}>
+          <Menu list={menuList} />
+        </Grid>
+      </BoxAdapter>
+    </div>
+  );
+}
