a73d31a6699adca210a4909bffba4a68d183cb0e
saeed mohammdiha <saeed1440124@gmail.com>
2024-03-05 16:15:54 +0330

    fix bug svg to icon component

--- src/ui/htsc-components/SvgToIcon/index.tsx
+++ src/ui/htsc-components/SvgToIcon/index.tsx
@@ -7,9 +7,9 @@ type props = {
 
 export default function SvgToIcon({ icon, alt = "" }: props) {
   return (
-    <Grid sx={{ width: "24px", height: "24px" }}>
+    <Grid sx={{ width: "24px", height: "24px", flexShrink: 0 }}>
       <img
-        style={{ height: "100%" }}
+        style={{ width: "100%", height: "100%", objectFit: "contain" }}
         src={icon}
         alt={alt}
       />
