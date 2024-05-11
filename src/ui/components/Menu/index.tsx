ebc44853d8717af826e05bc2fad47235a210e88f
saeed mohammdiha <saeed1440124@gmail.com>
2024-05-11 16:17:23 +0330

    clean menu component

--- src/ui/components/Menu/index.tsx
+++ src/ui/components/Menu/index.tsx
@@ -1,6 +1,5 @@
 import { Divider, Grid, Typography, useMediaQuery, useTheme } from '@mui/material';
 import { useTranslation } from 'react-i18next';
-import { useNavigate } from 'react-router-dom';
 import MenuItem from './MenuItem';
 import { Props } from './type';
 
@@ -8,7 +7,6 @@ export default function Menu(props: Props) {
 	const { list } = props;
 	const theme = useTheme();
 	const { t } = useTranslation();
-	const navigate = useNavigate();
 	const matches = useMediaQuery(theme.breakpoints.down('sm'));
 
 	return (
@@ -59,37 +57,3 @@ export default function Menu(props: Props) {
 		</>
 	);
 }
-
-// <div key={index}>
-//   <Grid
-//     container
-//     justifyContent={"space-between"}
-//     sx={{ padding: matches ? "16px 0" : "24px 0", cursor: "pointer" }}
-//     onClick={(e) => {
-//       item.onClick?.(e);
-//       item.routeTo && navigate(item.routeTo);
-//     }}
-//   >
-//     <Grid>
-//       <Grid
-//         container
-//         gap={"8px"}
-//       >
-//         <span> {item.icon}</span>
-//         <span> {t(item.title)}</span>
-//       </Grid>
-//     </Grid>
-//     <Grid>
-//       {theme.direction == "rtl" ? (
-//         <ArrowBackIosNewIcon />
-//       ) : (
-//         <ArrowForwardIosIcon />
-//       )}
-//     </Grid>
-//   </Grid>
-//   {!matches ? (
-//     list.length - 1 == index ? null : (
-//       <Divider sx={{ width: "calc(100% - 32px)", margin: "auto" }} />
-//     )
-//   ) : null}
-// </div>
