ab9aafeaf2f6f6eaa405f17200d0473c5d79c4a9
saeed mohammdiha <saeed1440124@gmail.com>
2024-05-29 10:09:34 +0330

    refactor the Check new info to have to sepatare view

---
+++ src/ui/components/transferCheck/FirstPersonView/index.tsx
@@ -0,0 +1,113 @@
+import { Grid, useMediaQuery, useTheme } from '@mui/material';
+import useGetReasonCodes from 'business/hooks/cheque/Digital Cheque/useGetReasonCodes';
+import { Controller, useForm } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import CheckOverViewBox from 'ui/components/CheckOverviewBox';
+import Title from 'ui/components/Title';
+import BottomSheetSelect from 'ui/htsc-components/BottomSheetSelect';
+import BoxAdapter from 'ui/htsc-components/BoxAdapter';
+import ButtonAdapter from 'ui/htsc-components/ButtonAdapter';
+import Stepper from 'ui/htsc-components/Stepper';
+import TextareaAdapter from 'ui/htsc-components/TextareaAdapter';
+
+export default function FirstPersonView() {
+	const { t } = useTranslation();
+	const theme = useTheme();
+	const matches = useMediaQuery(theme.breakpoints.down('md'));
+    const { data: reasonCodes, isLoading: isPendingtoGetReasons } = useGetReasonCodes();
+
+	const { control, formState } = useForm();
+
+	return (
+		<BoxAdapter fullWidth={matches}>
+			<Grid
+				minHeight={matches ? 'calc(100vh - 64px)' : 'calc(100vh - 192px)'}
+				container
+				direction={'column'}
+				wrap="nowrap"
+				justifyContent={'space-between'}
+			>
+				<Grid
+					container
+					direction={'column'}
+					gap={'16px'}
+				>
+					<Title>{t('transferCheckEl')}</Title>
+					{!matches ? (
+						// TODO: check if selected compony or homself acocunt and add one more step if it is compony
+						<Stepper
+							list={[
+								t('checkInfo'),
+								t('recivers'),
+								t('verificationCode'),
+								t('selectSignatureGroup'),
+								t('end')
+							]}
+							active={0}
+						/>
+					) : null}
+
+					<CheckOverViewBox />
+					<Grid
+						item
+						xs={5}
+					>
+						<Controller
+							name="reason"
+							control={control}
+							render={({ field }) => (
+								<BottomSheetSelect
+									isRequired
+									label={t('reason')}
+									list={
+										[]
+										// isPendingtoGetReasons
+										// 	? []
+										// 	: reasonCodes!?.map((reason) => ({
+										// 			value: reason.reasonCode,
+										// 			name: reason.description
+										// 		}))
+									}
+									onChange={(item) => {
+										field.onChange(item);
+									}}
+									error={!!formState?.errors?.reason}
+									helperText={formState?.errors?.reason?.message as string}
+								/>
+							)}
+						/>
+					</Grid>
+					<Grid
+						item
+						xs={12}
+					>
+						<Controller
+							name="description"
+							control={control}
+							render={({ field }) => (
+								<TextareaAdapter
+									onChange={(value) => field.onChange(value)}
+									isRequired
+									label={t('description')}
+									error={!!formState?.errors?.description}
+									helperText={'formState?.errors?.description?.message'}
+								/>
+							)}
+						/>
+					</Grid>
+				</Grid>
+				<Grid container>
+					<ButtonAdapter
+						variant="contained"
+						size="medium"
+						muiButtonProps={{ sx: { width: '100%', marginTop: '16px' } }}
+						forwardIcon
+						onClick={() => console.log()}
+					>
+						{t('continue')}
+					</ButtonAdapter>
+				</Grid>
+			</Grid>
+		</BoxAdapter>
+	);
+}
--- src/ui/pages/receivedChecksList/TransferCheck/CheckNewInfo/index.tsx
+++ src/ui/pages/receivedChecksList/TransferCheck/CheckNewInfo/index.tsx
@@ -1,32 +1,19 @@
 import { Grid, useMediaQuery, useTheme } from '@mui/material';
-import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import Menu from 'ui/components/Menu';
-import Title from 'ui/components/Title';
 import BoxAdapter from 'ui/htsc-components/BoxAdapter';
-import ButtonAdapter from 'ui/htsc-components/ButtonAdapter';
-import Stepper from 'ui/htsc-components/Stepper';
 
-import useGetReasonCodes from 'business/hooks/cheque/Digital Cheque/useGetReasonCodes';
 import { useChecklistData } from 'business/stores/checklistData/checklistData';
 import { useEffect } from 'react';
-import { Controller, useForm } from 'react-hook-form';
-import CheckOverViewBox from 'ui/components/CheckOverviewBox';
-import BottomSheetSelect from 'ui/htsc-components/BottomSheetSelect';
-import TextareaAdapter from 'ui/htsc-components/TextareaAdapter';
 import Loader from 'ui/htsc-components/loader/Loader';
 import { menuList } from 'ui/pages/HomePage/menuList';
+import FirstPersonView from '../../../../components/transferCheck/FirstPersonView';
 
 export default function CheckNewInfo() {
 	const navigate = useNavigate();
-	const { t } = useTranslation();
 	const theme = useTheme();
 	const matches = useMediaQuery(theme.breakpoints.down('md'));
 	const { sayadNo } = useChecklistData((store) => store);
-	const { data: reasonCodes, isLoading: isPendingtoGetReasons } = useGetReasonCodes();
-
-	const { control, formState } = useForm();
-	console.log({ sayadNo });
 
 	useEffect(() => {
 		//call an api for checking if it is persional or company and if it is first person or not
@@ -45,94 +32,7 @@ export default function CheckNewInfo() {
 				xs={12}
 				md={8}
 			>
-				<BoxAdapter fullWidth={matches}>
-					<Grid
-						minHeight={matches ? 'calc(100vh - 64px)' : 'calc(100vh - 192px)'}
-						container
-						direction={'column'}
-						justifyContent={'space-between'}
-						wrap="nowrap"
-					>
-						<Grid
-							container
-							direction={'column'}
-							gap={'16px'}
-						>
-							<Title>{t('activationElCheck')}</Title>
-							{!matches ? (
-								// TODO: check if selected compony or homself acocunt and add one more step if it is compony
-								<Stepper
-									list={[
-										t('checkInfo'),
-										t('recivers'),
-										t('verificationCode'),
-										t('selectSignatureGroup'),
-										t('end')
-									]}
-									active={0}
-								/>
-							) : null}
-							<CheckOverViewBox />
-							<Grid
-								item
-								xs={5}
-							>
-								<Controller
-									name="reason"
-									control={control}
-									render={({ field }) => (
-										<BottomSheetSelect
-											isRequired
-											label={t('reason')}
-											list={
-												isPendingtoGetReasons
-													? []
-													: reasonCodes!.map((reason) => ({
-															value: reason.reasonCode,
-															name: reason.description
-														}))
-											}
-											onChange={(item) => {
-												field.onChange(item);
-											}}
-											error={!!formState?.errors?.reason}
-											helperText={formState?.errors?.reason?.message as string}
-										/>
-									)}
-								/>
-							</Grid>
-							<Grid
-								item
-								xs={12}
-							>
-								<Controller
-									name="description"
-									control={control}
-									render={({ field }) => (
-										<TextareaAdapter
-											onChange={(value) => field.onChange(value)}
-											isRequired
-											label={t('description')}
-											error={!!formState?.errors?.description}
-											helperText={'formState?.errors?.description?.message'}
-										/>
-									)}
-								/>
-							</Grid>
-						</Grid>
-						<Grid container>
-							<ButtonAdapter
-								variant="contained"
-								size="medium"
-								muiButtonProps={{ sx: { width: '100%', marginTop: '16px' } }}
-								forwardIcon
-								onClick={() => console.log()}
-							>
-								{t('continue')}
-							</ButtonAdapter>
-						</Grid>
-					</Grid>
-				</BoxAdapter>
+				<FirstPersonView />
 			</Grid>
 
 			{matches ? null : (
