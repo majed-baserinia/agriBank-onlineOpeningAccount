c7555aaf0a45c6e17732cd457c7c9daa99aaba1c
saeed mohammdiha <saeed1440124@gmail.com>
2024-08-17 13:29:35 +0330

    refactor: complete the third party page

--- src/ui/pages/ThirdPartyAuthPage/index.tsx
+++ src/ui/pages/ThirdPartyAuthPage/index.tsx
@@ -1,14 +1,27 @@
+import useCustomerDidKycOperation from 'business/hooks/useCustomerDidKycOperation';
 import { useDataSteps } from 'business/stores/onlineOpenAccount/dataSteps';
-import { useEffect, useRef } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Loader from 'ui/htsc-components/loader/Loader';
 import { paths } from 'ui/route-config/paths';
 
 export default function ThirdPartyAuthPage() {
 	const navigate = useNavigate();
-	const url = useRef('');
-	const { orderId } = useDataSteps();
+	const [url, setUrl] = useState('');
+	const { orderId, token } = useDataSteps();
+	const { mutate, isLoading } = useCustomerDidKycOperation();
 
 	useEffect(() => {
+		async function createUrlByOrderId() {
+			try {
+				const res = await fetch('/api-config-open-account.json');
+				const apiConf = await res.json();
+				const constructedUrl = `${apiConf.kycUrl}?orderId=${orderId}&redirectUrl=https://digitalbanking.bki.ir/onlineopeningaccount`;
+				setUrl(constructedUrl);
+			} catch {
+				console.log("can't get the config file ");
+			}
+		}
 		createUrlByOrderId();
 	}, []);
 
@@ -19,31 +32,43 @@ export default function ThirdPartyAuthPage() {
 		};
 	}, []);
 
-	async function createUrlByOrderId() {
-		try {
-			const res = await fetch('/onlineOpeningAccount/api-config.json');
-			const apiConf = await res.json();
-			const constructedUrl = `${apiConf.kycUrl}?orderId=${orderId}`;
-			url.current = constructedUrl;
-		} catch {
-			console.log("can't get the config file ");
-		}
-	}
-
 	const getResultFromWebView = (e: MessageEvent) => {
 		if (e.data.event_id === 'kycResponse') {
 			// اتمام کار شرکت شاکلید
-			navigate(paths.result);
+			e.data.data.kycStatus === 'unAthorized';
+			e.data.data.kycStatus === 'InvalidOrderId';
+			e.data.data.kycStatus === 'InvalidKYCStatus';
+			e.data.data.kycStatus === 'Approved';
+			e.data.data.kycStatus === 'Reject';
+			e.data.data.kycStatus === 'Draft';
+			e.data.data.kycStatus === 'InProgress';
+			if (e.data.data.kycStatus === 'unAthorized') {
+			}
+			if (e.data.data.kycStatus === 'Approved' || e.data.data.kycStatus === 'Reject') {
+				mutate(
+					{ token: token! },
+					{
+						onSuccess: (res) => {
+							navigate(paths.result, { state: { status: e.data.data.kycStatus } });
+						},
+						onError: (err) => {
+							navigate(paths.result, { state: { status: e.data.data.kycStatus } });
+						}
+					}
+				);
+			}
 		}
 	};
 
-	return (
+	return url ? (
 		<iframe
 			style={{ width: '100vw', height: '100vh', border: 'none' }}
 			allow="camera *;microphone *"
 			scrolling="auto"
-			src={url.current}
+			src={url}
 			allowFullScreen
 		></iframe>
+	) : (
+		<Loader showLoader />
 	);
 }
