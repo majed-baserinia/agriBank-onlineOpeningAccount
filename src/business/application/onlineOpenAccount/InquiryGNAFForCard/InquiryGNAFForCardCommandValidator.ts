71450240a6e2bd9dee34a10472b3f4793e65133b
saeed mohammdiha <saeed1440124@gmail.com>
2024-08-31 12:20:09 +0330

    fix: add validations for forms

--- src/business/application/onlineOpenAccount/InquiryGNAFForCard/InquiryGNAFForCardCommandValidator.ts
+++ src/business/application/onlineOpenAccount/InquiryGNAFForCard/InquiryGNAFForCardCommandValidator.ts
@@ -1,11 +1,12 @@
 import fluentValidator from '@Fluentvalidator/attributes/validate.attribute';
 import { Validator } from '@Fluentvalidator/index';
+import i18next from 'i18next';
 import InquiryGNAFForCardCommand from './InquiryGNAFForCardCommand';
 
 @fluentValidator(InquiryGNAFForCardCommand)
 export class InquiryGNAFForCardCommandValidator extends Validator<InquiryGNAFForCardCommand> {
 	constructor() {
 		super();
-		//TODO: add validation
+		this.ruleFor('postalCode').notNull().withMessage(i18next.t('notEmptypostalcodeText').toString());
 	}
 }
