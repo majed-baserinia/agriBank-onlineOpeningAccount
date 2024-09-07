c36238a4f4d75b8f79e6194d79e03dc61ec63d64
saeed mohammdiha <saeed1440124@gmail.com>
2024-09-07 10:07:36 +0330

    refactor: add the config to check if there is a need to init postmessage

--- src/business/hooks/postMessage/useInitPostMessage.ts
+++ src/business/hooks/postMessage/useInitPostMessage.ts
@@ -1,5 +1,7 @@
+import { pushAlert } from 'business/stores/AppAlertsStore';
 import useInitialSettingStore, { InitialSetting } from 'business/stores/initial-setting-store';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef } from 'react';
+import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import { postMessageTypes } from './types';
 
@@ -14,59 +16,104 @@ export const sendPostmessage = (type: postMessageTypes, data: string) => {
 
 const useInitPostMessage = () => {
 	const navigate = useNavigate();
+	const { t } = useTranslation();
 
-	const { settings, setSettings } = useInitialSettingStore((s) => s);
 	const needsInitData = import.meta.env.VITE_APP_NEEDS_INIT_POSTMESSAGE === 'true';
-	const [receivedInitPostmessage, setReceivedInitPostMessage] = useState(!needsInitData);
+	const { settings, setSettings } = useInitialSettingStore((s) => s);
+
+	const receivedInitPostmessage = useRef(false);
+	const intervalRef = useRef<NodeJS.Timeout | null>(null);
 
 	useEffect(() => {
-		window.addEventListener('message', onRecievePostMessage);
-		let interval: NodeJS.Timeout;
-
-		if (checkIsInIframe() && !receivedInitPostmessage) {
-			let sendLimit = 20;
-			interval = setInterval(() => {
-				sendPostmessage('iFrameReady', 'Hi Parent');
-				sendLimit--;
-				if (sendLimit === 0 && !receivedInitPostmessage) {
-					sendPostmessage('isFinishedBack', 'true');
-					clearInterval(interval);
-				}
-			}, 300);
-		} else {
-			setReceivedInitPostMessage(true);
-		}
-
-		// Clean up the event listener on unmount
-		return () => {
-			window.removeEventListener('message', onRecievePostMessage);
-			if (interval) {
-				clearInterval(interval);
+		const onReceivePostMessage = (event: MessageEvent) => {
+			const type = event.data.type;
+			if (type === 'initiateIFrame') {
+				initiateIFrameHandler(event.data.data);
+			}
+			if (type === 'goback') {
+				goBackHandler();
 			}
 		};
-	}, []);
 
-	const onRecievePostMessage = (event: MessageEvent) => {
-		const type = event.data.type;
-		if (type === 'initiateIFrame') initiateIFrameHadnler(event.data.data);
-		if (type === 'goback') goBackHandler();
-	};
+		const startSendingPostMessages = () => {
+			if (receivedInitPostmessage.current) return; // Prevent sending if response is already received
 
-	const initiateIFrameHadnler = (data: InitialSetting) => {
-		setReceivedInitPostMessage(true);
-		setSettings(data);
+			const id = setInterval(() => {
+				if (receivedInitPostmessage.current) {
+					clearInterval(id);
+				} else {
+					sendPostmessage('iFrameReady', 'Hi Parent');
+				}
+			}, 500);
+
+			intervalRef.current = id;
+
+			// Schedule clearing of interval after 5 seconds
+			setTimeout(() => {
+				if (intervalRef.current) {
+					clearInterval(intervalRef.current);
+				}
+
+				if (!receivedInitPostmessage.current) {
+					pushAlert({
+						type: 'error',
+						messageText: t('initErrorText'),
+						hasConfirmAction: true,
+						actions: {
+							onConfirm() {
+								sendPostmessage('isFinishedBack', 'true');
+							},
+							onCloseModal() {
+								sendPostmessage('isFinishedBack', 'true');
+							}
+						}
+					});
+				}
+			}, 5000);
+		};
+
+		window.addEventListener('message', onReceivePostMessage);
+
+		if (needsInitData) {
+			startSendingPostMessages();
+		} else {
+			if (intervalRef.current) {
+				clearInterval(intervalRef.current);
+			}
+			receivedInitPostmessage.current = true;
+		}
+
+		// Clean up the event listener and interval on unmount
+		return () => {
+			window.removeEventListener('message', onReceivePostMessage);
+			if (intervalRef.current) {
+				clearInterval(intervalRef.current);
+			}
+		};
+	}, [receivedInitPostmessage.current, needsInitData]);
+
+	const initiateIFrameHandler = (data: InitialSetting) => {
+		if (intervalRef.current) {
+			clearInterval(intervalRef.current);
+		}
+		receivedInitPostmessage.current = true;
+		setSettings({
+			...settings,
+			...data
+		});
 	};
 
 	const goBackHandler = () => {
 		//set the "base" proberty in the viteconfig file to the base url of the project to access import.meta.env.BASE_URL
 		const basePath = import.meta.env.BASE_URL;
-		if (window.location.pathname == basePath) {
+
+		if (window.location.pathname == basePath || `${window.location.pathname}/` == basePath) {
 			sendPostmessage('isFinishedBack', 'true');
 		}
 		navigate(-1);
 	};
 
-	return { readyToLoad: receivedInitPostmessage, checkIsInIframe };
+	return { readyToLoad: receivedInitPostmessage.current, checkIsInIframe };
 };
 
 export default useInitPostMessage;
