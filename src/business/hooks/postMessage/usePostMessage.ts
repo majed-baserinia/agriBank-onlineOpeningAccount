944a3ecb0deec0e1c056a9f09be10ae18feeb0a6
saeed mohammdiha <saeed1440124@gmail.com>
2024-09-07 10:10:39 +0330

    feat: add a hook for listening and sending a message to parent

---
+++ src/business/hooks/postMessage/usePostMessage.ts
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+
+type Props = {
+	callback: (e: MessageEvent<any>) => void;
+	message?: any;
+};
+export default function usePostMessage(props: Props) {
+	const { callback, message } = props;
+
+	useEffect(() => {
+		window.addEventListener('message', callback, false);
+		message ? window.parent.postMessage(message, '*') : null;
+		return () => {
+			window.removeEventListener('message', callback);
+		};
+	});
+}
